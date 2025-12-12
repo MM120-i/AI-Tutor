efdb73ad83fb59dbada95e45f69918ab43144e0f
Mahim <mahimmarufuzzaman1234@gmail.com>
2025-12-12 16:38:34 -0500

    respective subscription features

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -125,3 +125,34 @@ export const getUserCompanions = async (userId: string) => {
 
   return data;
 };
+
+// subscription permissions
+export const newCompanionPermissions = async () => {
+  const { userId, has } = await auth();
+  const supabase = createSupabaseClient();
+  let limit = 0;
+
+  if (has({ plan: "pro" })) {
+    return true;
+  } else if (has({ plan: "3_companion_limit" })) {
+    limit = 3;
+  } else if (has({ plan: "10_companion_limit" })) {
+    limit = 10;
+  }
+
+  const { data, error } = await supabase
+    .from("companions")
+    .select("id", { count: "exact" })
+    .eq("author", userId);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  const companionCount = data?.length;
+  if (companionCount >= limit) {
+    return false;
+  } else {
+    return true;
+  }
+};
